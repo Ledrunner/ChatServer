bc4a365c49e00ff753d9a99fa237738b2d133002
Osman Mazinov <digital_inventor@mail.ru>
2017-05-10 23:31:30 +0300

    Table added
    T-Sql Query used.

--- ChatServerDBManager/ChatServerDBManager/DAL/UserManager.cs
+++ ChatServerDBManager/ChatServerDBManager/DAL/UserManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace ChatServerDBManager.DAL
 {
-    class UserManager
+    static class UserManager
     {
+        static string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
     }
 }
--- ChatServerSQLQuery.sql
+++ ChatServerSQLQuery.sql
@@ -1,1 +1,7 @@
-﻿
+﻿ CREATE TABLE [dbo].[ChatDB] 
+(
+  ID int PRIMARY KEY NOT NULL IDENTITY (1,1),  
+  NickName nvarchar(50),
+  MessageTime DateTimeOffset NOT NULL default SYSDATETIMEOFFSET(), -- Текущее системное время по таймзоне;
+  UserId int NOT NULL
+);
